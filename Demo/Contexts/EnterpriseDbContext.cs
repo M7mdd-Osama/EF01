e43f6cf37410483f9ff5fd29e35413e3d4808534
Mohamed Osama Ali <ana20332183@gmail.com>
2024-08-27 06:54:59 +0300

    Done

---
+++ Demo/Contexts/EnterpriseDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Contexts
+{
+    internal class EnterpriseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = . ; Database = Enterprise; Trusted_Connection = true ");
+        //optionsBuilder.UseSqlServer("Data source = . ; Initial Catalog = Enterprise; Integrated Security = true "); //Old
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+
+    }
+}
